61ca20725826310610794785bfcd34fd4f7ee306
Ajam <AjamX101@gmail.com>
2023-08-09 23:35:08 +0545

    Create install_deps.sh

---
+++ .github/scripts/install_deps.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+apk update
+apk add \
+    autoconf \
+    automake \
+    bash \
+    bison \
+    cmake \
+    coreutils \
+    curl \
+    file \
+    flex \
+    gawk \
+    git \
+    gmp-dev \
+    jq \
+    libffi-dev \
+    libtool \
+    libunistring-dev \
+    lttng-ust-dev \
+    make \
+    patch \
+    perl \
+    qemu-aarch64 \
+    qemu-arm \
+    qemu-ppc \
+    qemu-ppc64 \
+    rsync \
+    texinfo \
+    util-linux \
+    wget \
+    zip 
